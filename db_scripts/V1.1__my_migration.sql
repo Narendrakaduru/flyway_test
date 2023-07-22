7b7509d52b657b72f4a88cacfac1423bb0bf32c5
nanidevops <nanidevops999@gmail.com>
2023-07-22 16:40:11 +0530

    first commit

---
+++ db_scripts/V1.1__my_migration.sql
@@ -0,0 +1,2 @@
+INSERT INTO student VALUES(1004,'gani');
+INSERT INTO student VALUES(1005,'raja');
---
+++ db_scripts/V1__my_migration.sql
@@ -0,0 +1,2 @@
+INSERT INTO student VALUES(1002,'deva');
+INSERT INTO student VALUES(1003,'naga');
