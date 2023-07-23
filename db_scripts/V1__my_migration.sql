05207ea6797d799540efbaba78e65d457db5cdfb
Narendrakaduru <kadurunarendra@gmail.com>
2023-07-23 19:05:08 +0530

    Update V1__my_migration.sql

--- db_scripts/V1__my_migration.sql
+++ db_scripts/V1__my_migration.sql
@@ -1,2 +1,3 @@
-INSERT INTO student VALUES(1002,'deva');
-INSERT INTO student VALUES(1003,'naga');
+INSERT INTO employee VALUES(1001,'nani');
+INSERT INTO employee VALUES(1002,'deva');
+INSERT INTO employee VALUES(1003,'naga');
